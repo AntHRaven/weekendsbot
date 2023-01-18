9db6e088b079a5e8f515c4b28fec399ade929705
Nikita Veko <n.veko@glovisrus.com>
2023-01-18 16:30:24 +0300

    init

---
+++ src/components/toolBar/desktopToolBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-toolbar class="desktop-only">
+    <q-toolbar-title style="max-width: 145px"> Weekendsbot </q-toolbar-title>
+    <q-tabs v-model="tab" shrink stretch>
+      <q-tab name="tab1" label="Tab 1" />
+      <q-tab name="tab2" label="Tab 2" />
+      <q-tab name="tab3" label="Tab 3" />
+      <q-btn-dropdown auto-close stretch flat label="More">
+        <q-list>
+          <q-item clickable @click="tab = 'movies'">
+            <q-item-section>Movies</q-item-section>
+          </q-item>
+
+          <q-item clickable @click="tab = 'photos'">
+            <q-item-section>Photos</q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+    </q-tabs>
+  </q-toolbar>
+</template>
+
+<script>
+export default {
+  name: "desktopToolBar",
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/toolBar/mobileToolBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-footer elevated class="bg-white no-shadow">
+    <q-toolbar class="desktop-only">
+      <q-toolbar-title>Footer</q-toolbar-title>
+    </q-toolbar>
+    <q-toolbar style="padding-left: 0" class="mobile-only">
+      <q-tabs
+        style="min-width: 100vw"
+        align="center"
+        shrink
+        stretch
+        v-model="tab"
+        class="bg-white text-grey"
+      >
+        <q-tab name="search" icon="search" label="Поиск" />
+        <q-tab name="map" icon="map" label="Регионы" />
+        <q-tab name="profile" icon="person" label="Профиль" />
+      </q-tabs>
+    </q-toolbar>
+  </q-footer>
+</template>
+
+<script>
+export default {
+  name: "mobileToolBar",
+};
+</script>
+
+<style scoped></style>
